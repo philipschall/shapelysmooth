29c97c4daac0dd314b032f112a79a8d42ceae4e9
Taher Chegini <cheginit@gmail.com>
2024-03-22 18:48:43 -0400

    LNT: Run pre-commit.

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 import os
 import sys
+
 DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.join(DIR, 'external', 'pybind11'))
+sys.path.append(os.path.join(DIR, "external", "pybind11"))
 
 from glob import glob
-from setuptools import setup
+
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 
 del sys.path[-1]
 
 ext_modules = [
-    Pybind11Extension('_shapelysmooth',
-        include_dirs = ['./src'],
-        sources = sorted(glob('src/*.c*')),
-        define_macros = [('_shapelysmooth', '_shapelysmooth')],
-		extra_compile_args = ['-O3', '-Wall'],
-        ),
+    Pybind11Extension(
+        "_shapelysmooth",
+        include_dirs=["./src"],
+        sources=sorted(glob("src/*.c*")),
+        define_macros=[("_shapelysmooth", "_shapelysmooth")],
+        extra_compile_args=["-O3", "-Wall"],
+    ),
 ]
 
 setup(
